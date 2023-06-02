2ca7aece50170cd6a0d7d72884d2eeac8c1edeb6
Isadora Silva <isasilva123ptc@gmail.com>
2023-06-02 20:14:26 -0300

    slide multi cards

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ function nextImage() {
 
     document.getElementById("radio" + count).checked = true;
 }
+
+$('#recipeCarousel').carousel({
+    interval: 10000
+})
+
+$('.carousel .carousel-item').each(function () {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
